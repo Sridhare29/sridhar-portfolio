4280ae7fecbae53ec8c82d9d8c083e477cd61e10
Sridhar <sridhar.elumalai@kanini.com>
2025-05-17 20:15:29 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/pages/Dashboard/Footer.jsx
+++ src/components/pages/Dashboard/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
     <footer className="w-full py-6 mt-auto bg-gray-100 ">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center space-y-2">
-          <p className="text-gray-700  text-sm">
+          {/* <p className="text-gray-700  text-sm">
             © {currentYear} All rights reserved
-          </p>
+          </p> */}
           <div className="flex items-center space-x-1 text-gray-900 ">
             <span>Developed by Sridhar ❤️</span>
           </div>
